6a72e7afce2716242bd19276adc487a3cbaf66bd
Mike Purvis <mikedotexe@gmail.com>
2020-06-16 21:03:47 -0700

    add script to bloat manually

---
+++ upgrade-way/bloat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+near call upgrade.$NEAR_ACCT bloat_map --accountId upgrade.$NEAR_ACCT --gas 10000000000000000
+
